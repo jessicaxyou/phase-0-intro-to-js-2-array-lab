74d3954f4c82af29eb8dba5f0e1597e698702d9e
jessica you <jessicaxytan@gmail.com>
2021-05-04 21:18:16 -0400

    commit tree 49cfcb9f23f63769682c5505280da2dfcd621569

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+// appends a cat to the end of the cats array
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph")
+}
+
+// prepends a cat to the beginning of the cats array
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob")
+}
+
+// removes the last cat from the cats array:
+function destructivelyRemoveLastCat(name) {
+    return cats.pop()
+}
+
+// removes the first cat from the cats array:
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift()
+}
+
+// appends a cat to the cats array and returns a new array, leaving the cats array unchanged:
+function appendCat(name) {
+    const newCats = [...cats]
+    newCats.push("Broom")
+    return newCats
+}
+
+// prepends a cat to the cats array and returns a new array, leaving the cats array unchanged:
+function prependCat(name) {
+    const newCats = [...cats]
+    newCats.unshift("Arnold")
+    return newCats
+}
+
+// removes the last cat in the cats array and returns a new array, leaving the cats array unchanged:
+function removeLastCat() {
+    const newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+function removeFirstCat() {
+    const newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
+
+
+// removes the first cat from the cats array and returns a new array, leaving the cats array unchanged:
